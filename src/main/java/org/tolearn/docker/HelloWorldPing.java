3d303086af7155598049b137e3ae048e0b4c308c
jafortes <61018644+jafortes@users.noreply.github.com>
2020-04-24 19:03:14 +0100

    First Docker Project Commit(push/upload)

---
+++ src/main/java/org/tolearn/docker/HelloWorldPing.java
@@ -0,0 +1,11 @@
+package org.tolearn.docker;
+
+public class HelloWorldPing {
+	
+	public static void main(String args[]) throws Exception{
+		for(int i = 0 ; i < 10; i++){
+			System.out.println("Hello World Ping " + i );
+			Thread.sleep(1000);
+		}
+	}
+}
