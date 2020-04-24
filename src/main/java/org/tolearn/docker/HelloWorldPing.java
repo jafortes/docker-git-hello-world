6ee38d315ba9a30bc594b0efac42567630a472fc
jafortes <61018644+jafortes@users.noreply.github.com>
2020-04-24 19:39:19 +0100

    Test Github to Docker Deploy

--- src/main/java/org/tolearn/docker/HelloWorldPing.java
+++ src/main/java/org/tolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 10; i++){
-			System.out.println("Hello World Ping " + i );
+			System.out.println("Hello World Ping 2020 " + i );
 			Thread.sleep(1000);
 		}
 	}
