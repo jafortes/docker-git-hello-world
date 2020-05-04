3ef62bd39f5a1e1026ef58b601f883f12ed3b7aa
jafortes <61018644+jafortes@users.noreply.github.com>
2020-05-04 19:27:28 +0100

    First commit to merge branch and master

--- src/main/java/org/tolearn/docker/HelloWorldPing.java
+++ src/main/java/org/tolearn/docker/HelloWorldPing.java
@@ -4,7 +4,7 @@ public class HelloWorldPing {
 	
 	public static void main(String args[]) throws Exception{
 		for(int i = 0 ; i < 10; i++){
-			System.out.println("Hello World Ping 2020:" + i );
+			System.out.println("Hello World Ping:" + i );
 			Thread.sleep(1000);
 		}
 	}
